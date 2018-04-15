a6a9cfbc54d604b05df7515f5cda74b184dae6b3
Haddadson <gabriel.haddad15@gmail.com>
2018-04-14 21:04:35 -0300

    Some changes into mobile layout

--- assets/js/craft-my-pc.js
+++ assets/js/craft-my-pc.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $("#start-button").on('click', function(){
         $("#conteudo-incluso").empty();
         $("#conteudo-incluso").load("components/initial-question.html");
+        $(".craft-intro").remove();
     });
 
    $("#conteudo-incluso").on('click','#jogo' ,function(){
