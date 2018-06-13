d41b42cf249fa49e55d291df967f8424a7f9b347
Haddadson <gabriel.haddad15@gmail.com>
2018-06-13 00:47:37 -0300

    Updates query to return computer price

--- backend.php
+++ backend.php
@@ -7,7 +7,7 @@ $parametro2 = $_REQUEST['param2'];
 require_once 'conexao.php';
 
 
-$sql = "SELECT map.txt_descricao, pc.nom_placaMae, pc.nom_processador, pc.nom_placaVideo, pc.qtd_ram, pc.qtd_armazenamento, pc.nom_fonte from mapeamento map INNER JOIN computador pc ON map.cod_pc = pc.cod_pc AND map.cod_config = pc.cod_config where map.cod_escolha1 = '".$parametro0."' and map.cod_escolha2 ='".$parametro1."'and map.cod_escolha3 ='".$parametro2."'";
+$sql = "SELECT map.txt_descricao, pc.nom_placaMae, pc.nom_processador, pc.nom_placaVideo, pc.qtd_ram, pc.qtd_armazenamento, pc.nom_fonte, pc.vlr_preco from mapeamento map INNER JOIN computador pc ON map.cod_pc = pc.cod_pc AND map.cod_config = pc.cod_config where map.cod_escolha1 = '".$parametro0."' and map.cod_escolha2 ='".$parametro1."'and map.cod_escolha3 ='".$parametro2."'";
 $dados = [];
 
 $p_sql = Conexao::getInstance()->prepare($sql);
